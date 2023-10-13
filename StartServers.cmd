657c154824152d023c50075a21661b61d23c984d
Daniel <daniel@familie-ryssel.de>
2023-10-13 22:26:24 +0200

    PROJ-14 <Ursprüngliche Version von StartServers.cmd erstellt>

---
+++ StartServers.cmd
@@ -0,0 +1,2 @@
+start cmd.exe /k "cd ..\venv\quanto\Scripts && activate && cd ..\..\..\quanto-project\backend\quantobackend && python manage.py runserver"
+start cmd.exe /k "cd frontend\quanto-front && npm run dev"
