43e45b56a71e998533cc0859e8eb2e1de1e9f530
Daniel <daniel@familie-ryssel.de>
2023-10-13 22:29:05 +0200

    PROJ-14 <Hinzufügen von Option auszuwählen welche Server gestartet werden>

--- StartServers.cmd
+++ StartServers.cmd
@@ -1,2 +1,20 @@
-start cmd.exe /k "cd ..\venv\quanto\Scripts && activate && cd ..\..\..\quanto-project\backend\quantobackend && python manage.py runserver"
-start cmd.exe /k "cd frontend\quanto-front && npm run dev"
+@echo off
+
+set "flag="
+
+:loop
+if "%1"=="" goto execute
+if "%1"=="-b" set "flag=backend"
+if "%1"=="-f" set "flag=frontend"
+shift
+goto loop
+
+:execute
+if "%flag%"=="backend" (
+    start cmd.exe /k "cd ..\venv\quanto\Scripts && activate && cd ..\..\..\quanto-project\backend\quantobackend && python manage.py runserver"
+) else if "%flag%"=="frontend" (
+    start cmd.exe /k "cd frontend\quanto-front && npm run dev"
+) else (
+    start cmd.exe /k "cd ..\venv\quanto\Scripts && activate && cd ..\..\..\quanto-project\backend\quantobackend && python manage.py runserver"
+    start cmd.exe /k "cd frontend\quanto-front && npm run dev"
+)
