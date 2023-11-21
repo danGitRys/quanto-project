ac8f20f694488d5c11092b4a42c73883de9302fd
danGitRys <daniel@familie-ryssel.de>
2023-11-21 15:17:00 +0100

    PROJ-56 <Email erfolgreich aus Token extrahiert>

--- backend/project_management_tool/views/login.py
+++ backend/project_management_tool/views/login.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from authlib import jose
 from ..jsonValidator import validator
 from ..jsonTemplate import *
 import json
@@ -10,9 +11,21 @@ def login(request):
     if request.method == "POST":
 
         if(validator.login(json.loads(request.body))):
-            data = {
-                "type":"Post"
-            }
+            try:
+                public = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArcTWSGwEAalCoWpGMO+G\nyzDkWUeRGWxp9FA5i7jhS2122lrlvMCEoJfuQ6WcsvnsA6Qed6X0bIY31JYuHqhO\nbJ/cR7I9LqI+VPgJatjlGkQAb/AHfD0oAtyAvSH7eFyuaRtE2piVx2+mvASETDnB\ndR2MvrrY3hrr3Te7NRNtAZs1CqhjJPONPCRrINuwlZ+USynhMe0Vju3ajmPDgEQZ\nvu4sBaWmfOA6d3h8pFulkW6VCNA00KTR9MjG4qJVSAYhlkR1bSeed1gIP13e0/h1\nRB7udUckgdjXOSdNwLg1sIxZgHsKmyxcr6eCGeKgXu7OXyHspi48nDBtYgwP/jOy\nkwIDAQAB\n-----END PUBLIC KEY-----"
+                print(request.body)
+                data = json.loads(request.body)
+                token = data["token"]
+                decoded = jose.jwt.decode(token,key=public)
+                email = decoded["email"]
+                print(email)
+                
+
+
+            
+            except:
+                print("fail")
+
         else:
              data = {
                 "Error":"Invalid Json"
