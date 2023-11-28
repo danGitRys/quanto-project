67be9df1072ebc57e7e486666c44187c1a815cf7
danGitRys <daniel@familie-ryssel.de>
2023-11-28 16:49:56 +0100

    Temp Commit

---
+++ frontend/quanto-front/src/components/demo/getTeamComponent.vue
@@ -0,0 +1,44 @@
+<template>
+<h1>
+    Test
+</h1>
+</template>
+
+<script>
+import axios from "axios"
+import { getBaseTransformPreset } from '@vue/compiler-core';
+
+
+export default {
+    
+    data() {
+        return {
+            drawer: true,
+            rail: true,
+        }
+    },
+
+    methods:{
+        getTeam(){
+            console.log("test")
+            axios.get("http://localhost:8000/getTeam/"+this.$route.params.id,{
+           
+        }).then(response => {
+            console.log(response)
+            
+        })
+        .catch(error=> {
+            console.log(error)
+            alert("Invalid Login")
+        })
+        }
+    },
+
+    beforeMount(){
+        this.getTeam()
+    }
+
+
+};
+
+</script>
