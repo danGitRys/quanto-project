f83e2680c08bff3fb3cd8393f1f2095c6944ad4d
danGitRys <daniel@familie-ryssel.de>
2023-11-30 22:42:34 +0100

    View ordner umstrukturiert, create routes in post ordner verschoben

--- backend/project_management_tool/views/post/project.py
+++ backend/project_management_tool/views/post/project.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from ..models import Project
-from ..middleware import validator
+from ...models import Project
+from ...middleware import validator
 import json
 
 @csrf_exempt
