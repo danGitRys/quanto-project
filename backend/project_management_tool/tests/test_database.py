e3a6df1b6490a6e9187c3b05bc9223481d388743
danGitRys <daniel@familie-ryssel.de>
2023-11-30 23:18:25 +0100

    Abändern Tests

--- backend/project_management_tool/tests/test_database.py
+++ backend/project_management_tool/tests/test_database.py
@@ -7,8 +7,5 @@ class DateTestClass(TestCase):
     def test_First(self):
         testObject = Team.objects.create(name="Test",info="Test")
         testObject.save()
-        result = Team.objects.all()
-        for res in result:
-            print("--------------")
-            print(res.toJson())
-        self.assertEqual(True,True)
+        result:bool = Team.objects.filter(name="Test").exists()
+        self.assertEqual(True,result)
