a7e81c29f22e72bf6c777f3da650c9764a7f632b
Miche <mipoit01@hs-esslingen.de>
2023-12-03 16:34:51 +0100

    PROJ-62 < get-Route im Verzeichnis implementiert >

--- backend/project_management_tool/urls.py
+++ backend/project_management_tool/urls.py
@@ -36,8 +36,8 @@ urlpatterns = [
     path('getAssignment/<int:id>', getAssignment),
     path('assignedProjects/<int:id>', getAssignedProjects),
     path('getPositionsToProjectId/<int:id>', getPositionsToProjectId),
-    path('getEmployeesToProjectId/<int:id>', getEmployeesToProjectId)
-
+    path('getEmployeesToProjectId/<int:id>', getEmployeesToProjectId),
+    path('getPositionsToProjectId/<int:id>', getPositionsToProjectId)
 
 
 ]
