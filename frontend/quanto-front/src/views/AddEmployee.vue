c49875c018f2e65c78cbf32dffad114001741976
e2Neo <fabiansonek@live.deC:/Users/Anwender/anaconda3/Scripts/activate>
2023-12-08 17:07:38 +0100

    cleanup addEmployee code

--- frontend/quanto-front/src/views/AddEmployee.vue
+++ frontend/quanto-front/src/views/AddEmployee.vue
@@ -1,11 +1,14 @@
 <template>
-   <Default />
-    <AddEmployee />  
+    <Default />
+    <AddEmployee />
+
 </template>
 
 <script setup>
+import Submit from '@/layouts/default/Submit.vue';
 import AddEmployee from '../components/AddEmployee.vue';
 import Default from '@/layouts/default/Default.vue';
+
 </script>
 
 <style>
