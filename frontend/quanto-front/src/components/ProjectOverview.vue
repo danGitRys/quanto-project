7cbb202d48d1a5df53fa31c95971bbaa269fd2bf
Miche <mipoit01@hs-esslingen.de>
2023-12-15 00:57:23 +0100

    Änderungen fertiggestellt

--- frontend/quanto-front/src/components/ProjectOverview.vue
+++ frontend/quanto-front/src/components/ProjectOverview.vue
@@ -3,20 +3,66 @@
     <div class="custom-search-input">
       <InputText v-model="globalFilter" placeholder="Search..." />
     </div>
-    <DataTable :value="projects" paginator :rows="10" dataKey="id" :globalFilter="globalFilter">
+      <!-- DataTable-Komponente mit verschiedenen Spalten -->
+    <DataTable
+      :value="projects"
+      paginator
+      :rows="10"
+      dataKey="id"
+      :globalFilter="globalFilter"
+    >
+     <!-- Benutzerdefinierte Anzeige, wenn keine Projekte gefunden werden -->
       <template #empty>No projects found.</template>
 
-      <Column field="project_id" header="Project ID" sortable style="min-width: 10rem"></Column>
-      <Column field="project_name" header="Project Name" sortable style="min-width: 14rem"></Column>
-      <Column field="project_company" header="Company" sortable style="min-width: 14rem"></Column>
-      <Column field="project_start_date" header="Start Date" sortable style="min-width: 14rem"></Column>
-      <Column field="project_end_date" header="End Date" sortable style="min-width: 14rem"></Column>
-      <Column field="assignment_role" header="Role" sortable style="min-width: 14rem"></Column>
+      <!-- Definition von Spalten mit verschiedenen Feldern und Überschriften -->
+      <Column
+        field="project_id"
+        header="ID"
+        sortable
+        style="min-width: 10rem"
+      ></Column>
+      <Column
+        field="project_pid"
+        header="Project ID"
+        sortable
+        style="min-width: 14rem"
+      ></Column>
+      <Column
+        field="project_name"
+        header="Project Name"
+        sortable
+        style="min-width: 14rem"
+      ></Column>
+      <Column
+        field="project_company"
+        header="Company"
+        sortable
+        style="min-width: 14rem"
+      ></Column>
+      <Column
+        field="assignment_role"
+        header="Role"
+        sortable
+        style="min-width: 14rem"
+      ></Column>
+      <Column
+        field="project_start_date"
+        header="Start Date"
+        sortable
+        style="min-width: 14rem"
+      ></Column>
+      <Column
+        field="project_end_date"
+        header="End Date"
+        sortable
+        style="min-width: 14rem"
+      ></Column>
     </DataTable>
   </div>
 </template>
 
 <style>
+/* Stildefinitionen für die Komponente */
 .card {
   border: 1px solid #ddd;
   border-radius: 8px;
@@ -37,6 +83,7 @@
   padding: 8px;
 }
 
+/* Stildefinitionen für die DataTable-Elemente */
 .p-datatable-thead th {
   background-color: #f2f2f2; /* Light gray background for headers */
   padding: 10px;
@@ -44,7 +91,7 @@
 }
 
 .p-datatable-tbody tr {
-  font-family: 'Arial', sans-serif; /* Change the font for entries */
+  font-family: "Arial", sans-serif; /* Change the font for entries */
   color: #333; /* Change the text color for entries */
 }
 
@@ -53,69 +100,61 @@
 }
 </style>
 
-
 <script>
-import axios from 'axios';
-import { ref, onMounted } from 'vue';
-import { FilterMatchMode, FilterOperator } from 'primevue/api';
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
-import InputText from 'primevue/inputtext';
-import Calendar from 'primevue/calendar';
-import InputNumber from 'primevue/inputnumber';
-import Dropdown from 'primevue/dropdown';
-import Tag from 'primevue/tag';
+// Importieren von benötigten Bibliotheken und Komponenten
+import axios from "axios";
+import { ref, onMounted } from "vue";
+import { FilterMatchMode, FilterOperator } from "primevue/api";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import InputText from "primevue/inputtext";
+import Calendar from "primevue/calendar";
+import InputNumber from "primevue/inputnumber";
+import Dropdown from "primevue/dropdown";
+import Tag from "primevue/tag";
 
 export default {
-
   data() {
+    // Datenoption für die Projekte
     return {
-      projects: []
-    }
+      projects: [],
+    };
   },
   methods: {
-
+     // Methode zum Abrufen von Projektdaten über eine API-Anfrage
     getProjects() {
-      console.log("test")
-      axios.get("http://localhost:8000/getProjectsForEmployee/" + this.$route.params.id, {
+      console.log("test");
+      axios
+        .get(
+          "http://localhost:8000/getProjectsForEmployee/" +
+            this.$route.params.id,
+          {}
+        )
+        .then((response) => {
+          var responseData = response.data;
+          var valid = responseData.success;
+          if (valid == true) {
+            var tempData = responseData.data;
+            console.log(tempData);
 
-      }).then(response => {
-
-        var responseData = response.data
-        var valid = responseData.success
-        if (valid == true) {
-          var tempData = responseData.data
-          console.log(tempData)
-
-          for(var i=0;i<tempData.length;i++){
-                    var tempProject = tempData[i]
-                    this.projects.push(tempProject)
-
-
-                    }
-
-
-        }
-        else {
-          alert("This Team doesnt exist")
-        }
-
-
-      })
-        .catch(error => {
-          console.log(error)
-
+            // Iteration durch die erhaltenen Projektdaten und Hinzufügen zu 'projects'
+            for (var i = 0; i < tempData.length; i++) {
+              var tempProject = tempData[i];
+              this.projects.push(tempProject);
+            }
+          } else {
+            alert("This Team doesnt exist");
+          }
         })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-
-
-
-
   },
+  // Hook vor dem Mounten der Komponente, um Projektdaten zu laden
   beforeMount() {
-      this.getProjects()
-      console.log("Called")
-
-    }
-}
+    this.getProjects();
+    console.log("Called");
+  },
+};
 </script>
--- frontend/quanto-front/src/views/LoginScreen.vue
+++ frontend/quanto-front/src/views/LoginScreen.vue
@@ -1,196 +1,191 @@
 <script setup>
-import HelloWorld from '@/components/HelloWorld.vue'
-import TopBar from '@/components/TopBar.vue'
-import SideBar from '@/components/SideBarMitarbeiter.vue'
-import { ref, reactive } from 'vue';
-import {AuthService} from '@/service/login.js'
-import axios from "axios"
-import { useRouter } from 'vue-router';
+// Import der benötigten Vue-Komponenten und Funktionen
+import HelloWorld from "@/components/HelloWorld.vue";
+import TopBar from "@/components/TopBar.vue";
+import SideBar from "@/components/SideBarMitarbeiter.vue";
+import { ref, reactive } from "vue";
+import { AuthService } from "@/service/login.js";
+import axios from "axios";
+import { useRouter } from "vue-router";
 let isPassword = ref(true);
 function showPassword() {
-    isPassword.value = !isPassword.value;
+  isPassword.value = !isPassword.value;
 }
-const email = ref('');
-const password = ref('');
+const email = ref("");
+const password = ref("");
 
-
-// 
+//
 async function getEmployeeData() {
-    // const url = "http://localhost:3001/"
-    // fetch(url).then(response => response.json())
-    // .then(data => console.log(data))
-    // .catch(err => console.log(err))
-    console.log("Called")
-    console.log(email.value)
-    var test = undefined
-    const router = useRouter(); 
-    try {
-        const token = await AuthService.login(email.value, password.value);
-        console.log("here comes the token")
-        console.log(token);
-        axios.post("http://localhost:8000/login",{
-            token:token
-        }).then(response => {
-            console.log(response)
-            if(response.data['login']==true){
-                window.location.href = '/dataTable';
-            }
-            else{
-                alert("Invalid Login")
-            }
-        })
-        .catch(error=> {
-            console.log(error)
-            alert("Invalid Login")
-        })
-    } catch (error) {
-        console.error(error);
+  // const url = "http://localhost:3001/"
+  // fetch(url).then(response => response.json())
+  // .then(data => console.log(data))
+  // .catch(err => console.log(err))
+  console.log("Called");
+  console.log(email.value);
+  var test = undefined;
+  const router = useRouter();
+  try {
+    const token = await AuthService.login(email.value, password.value);
+    console.log("here comes the token");
+    console.log(token);
+    axios
+      .post("http://localhost:8000/login", {
+        token: token,
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.data["login"] == true) {
+          window.location.href = "/dataTable";
+        } else {
+          alert("Invalid Login");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
         alert("Invalid Login");
-    }
-    
-    
-    
-   
-
+      });
+  } catch (error) {
+    console.error(error);
+    alert("Invalid Login");
+  }
 }
-
-
-
 </script>
 
 <template>
-    <div class="limiter">
-        <div class="loginScreenMainContainer">
-            <p class="loginTag">Login</p>
-            <div class=" loginScreenInputContainer">
-                <input v-model="email" id="inputEmployeeName" type="text" placeholder="E-Mail">
-                <div class="passwordContainer">
-                    <input v-model="password" :type="isPassword ? 'password' : 'text'" id="inputEmployeePassword"
-                        placeholder="Password">
-                    <div @click="showPassword" id="showPassword">
-                        <link rel="stylesheet"
-                            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-                        <i class="fas fa-eye" :class="{ 'fa-eye-slash': !isPassword }"></i>
-                    </div>
-
-                </div>
-                <button @click="getEmployeeData" id="loginButton">LOGIN</button>
-            </div>
-
-            <div class="logoContainer">
-                <div class="logoImage">
-                    <img src="../icons/LogoQuantoSolutions.png" alt="logo" id="logo">
-                </div>
-            </div>
-
+  <div class="limiter">
+    <div class="loginScreenMainContainer">
+      <p class="loginTag">Login</p>
+      <div class="loginScreenInputContainer">
+        <input
+          v-model="email"
+          id="inputEmployeeName"
+          type="text"
+          placeholder="E-Mail"
+        />
+        <div class="passwordContainer">
+          <input
+            v-model="password"
+            :type="isPassword ? 'password' : 'text'"
+            id="inputEmployeePassword"
+            placeholder="Password"
+          />
+          <div @click="showPassword" id="showPassword">
+            <link
+              rel="stylesheet"
+              href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+            />
+            <i class="fas fa-eye" :class="{ 'fa-eye-slash': !isPassword }"></i>
+          </div>
         </div>
+        <button @click="getEmployeeData" id="loginButton">LOGIN</button>
+      </div>
 
-
-
+      <div class="logoContainer">
+        <div class="logoImage">
+          <img src="../icons/LogoQuantoSolutions.png" alt="logo" id="logo" />
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
 .logoImage {
-    position: absolute;
-    top: 25%;
-    left: 35%;
+  position: absolute;
+  top: 25%;
+  left: 35%;
 }
 
 .limiter {
-    max-height: 100vh;
+  max-height: 100vh;
 }
 
 .logoContainer {
-    display: inline-block;
-    width: 35%;
-    height: 500px;
-    position: relative;
+  display: inline-block;
+  width: 35%;
+  height: 500px;
+  position: relative;
 }
 
-
 .passwordContainer {
-    position: relative;
-    width: 300px;
+  position: relative;
+  width: 300px;
 }
 
 .loginScreenMainContainer {
-    background: linear-gradient(110deg, #304C5D 40%, #94B8C7 40%);
-    min-height: 100vh;
-    min-width: 100vh;
+  background: linear-gradient(110deg, #304c5d 40%, #94b8c7 40%);
+  min-height: 100vh;
+  min-width: 100vh;
 }
 
 .loginTag {
-    font-size: 48px;
-    text-decoration: underline;
-    position: relative;
-    top: 200px;
-    left: 60%
+  font-size: 48px;
+  text-decoration: underline;
+  position: relative;
+  top: 200px;
+  left: 60%;
 }
 
 .loginScreenInputContainer {
-    display: flex;
-    flex-direction: column;
-    position: relative;
-    top: 300px;
-    left: 50%;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  top: 300px;
+  left: 50%;
 }
 
 #inputEmployeeName {
-    width: 300px;
-    height: 40px;
-    border: 2px solid black;
-    font-size: 24px;
-    outline-color: #EF7C00;
+  width: 300px;
+  height: 40px;
+  border: 2px solid black;
+  font-size: 24px;
+  outline-color: #ef7c00;
 }
 
 #inputEmployeeName::placeholder {
-    color: #304C5D;
-    padding-left: 10px;
+  color: #304c5d;
+  padding-left: 10px;
 }
 
 #inputEmployeePassword {
-    width: 300px;
-    height: 40px;
-    border: 2px solid black;
-    font-size: 24px;
-    margin-top: 20px;
-    padding-right: 40px;
-    /*reserviert Platz für Passwort anschauen Icon*/
-    outline-color: #EF7C00;
-
-
+  width: 300px;
+  height: 40px;
+  border: 2px solid black;
+  font-size: 24px;
+  margin-top: 20px;
+  padding-right: 40px;
+  /*reserviert Platz für Passwort anschauen Icon*/
+  outline-color: #ef7c00;
 }
 
 #inputEmployeePassword::placeholder {
-    color: #304C5D;
-    padding-left: 10px;
+  color: #304c5d;
+  padding-left: 10px;
 }
 
 #showPassword {
-    display: flex;
-    justify-content: center;
-    width: 30px;
-    position: absolute;
-    top: 31px;
-    right: 20px;
-    cursor: pointer;
+  display: flex;
+  justify-content: center;
+  width: 30px;
+  position: absolute;
+  top: 31px;
+  right: 20px;
+  cursor: pointer;
 }
 
-
 #loginButton {
-    width: 300px;
-    height: 40px;
-    font-size: 24px;
-    margin-top: 20px;
-    background-color: #304C5D;
-    color: #EF7C00;
-    font-weight: bold;
-    cursor: pointer;
-    border-radius: 10%;
+  width: 300px;
+  height: 40px;
+  font-size: 24px;
+  margin-top: 20px;
+  background-color: #304c5d;
+  color: #ef7c00;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 10%;
 }
 
 #loginButton:hover {
-    border: 2px solid #EF7C00;
+  border: 2px solid #ef7c00;
 }
 </style>
