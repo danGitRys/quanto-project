6985b17ef39361eea3837dbd17a0d3c00d777d05
Miche <mipoit01@hs-esslingen.de>
2023-12-15 11:21:42 +0100

    < Aktualisierung >

--- backend/project_management_tool/views/get/getProjectsForEmployee.py
+++ backend/project_management_tool/views/get/getProjectsForEmployee.py
@@ -1,3 +1,4 @@
+# Importieren der benötigten Django-Module und benutzerdefinierten Klassen
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from ...models import Employee, Project, Assignment
@@ -5,6 +6,8 @@ from ...middleware import validator
 from ...middleware import assignedProjects
 import json
 
+
+# Dekorator, um CSRF-Schutz für die Funktion zu deaktivieren
 @csrf_exempt
 def getProjectsForEmployee(request, id: int) -> JsonResponse:
     """ Endpoint for getting Projects for an Employee out of the Database
@@ -21,11 +24,13 @@ def getProjectsForEmployee(request, id: int) -> JsonResponse:
         if success = True 
         JSON with Data of the Projects
     """
+    # Initialisierung der Antwortdaten
     response_data = {
         "success": False,
         "message": "",
     }
 
+    # Überprüfen, ob die Anfrage eine GET-Anfrage ist
     if request.method == 'GET':
         # Überprüfe, ob der Mitarbeiter existiert
         if not Employee.objects.filter(id=id).exists():
@@ -37,6 +42,7 @@ def getProjectsForEmployee(request, id: int) -> JsonResponse:
         projects_query = assignedProjects(id)
         projects_data = projects_query.toJsonTotal()
 
+        # Überprüfen, ob Projektdaten vorhanden sind
         if projects_data:
             response_data["success"] = True
             response_data["data"] = projects_data
@@ -44,7 +50,9 @@ def getProjectsForEmployee(request, id: int) -> JsonResponse:
             response_data["success"] = False
             response_data["message"] = "No Projects Exist for this Employee"
     else:
+        # Fehlerfall: Ungültige Anfragemethode
         response_data["success"] = False
         response_data["message"] = "Invalid Method"
 
+    # Rückgabe der Antwortdaten als JsonResponse
     return JsonResponse(response_data)
