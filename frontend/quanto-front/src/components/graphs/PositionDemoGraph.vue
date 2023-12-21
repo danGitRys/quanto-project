0e85258cbde3ef778c33f658286c56bd1aa2f553
danGitRys <daniel@familie-ryssel.de>
2023-12-21 11:46:34 +0100

    PROJ-120 <vuechartjs hinzugefügt>

--- frontend/quanto-front/src/components/graphs/PositionDemoGraph.vue
+++ frontend/quanto-front/src/components/graphs/PositionDemoGraph.vue
@@ -1,3 +1,22 @@
 <template>
     <h1>Hello</h1>
+    <canvas id="my-chart" width="500" height="300"></canvas>
 </template>
+<script>
+
+import Chart from 'chart.js';
+
+new Chart(document.getElementById('my-chart'), {
+  type: 'line',
+  data: {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+    datasets: [
+      {
+        label: '2018 Sales',
+        data: [300, 700, 450, 750, 450]
+      }
+    ]
+  }
+});
+
+</script>
